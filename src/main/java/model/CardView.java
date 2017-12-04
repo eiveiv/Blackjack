843160dd9712b83abf4774d68cfe2cf6390039ab
EivindS <eiv.steen@gmail.com>
2017-12-04 04:29:38 +0100

    La til cardview og prøvde vise det

---
+++ src/main/java/model/CardView.java
@@ -0,0 +1,30 @@
+package model;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class CardView extends StackPane{
+
+    public CardView(Card card) {
+        Text text = new Text();
+        text.setText(String.valueOf(card.getShortName()));
+
+        getChildren().addAll(createCard(), text);
+
+    }
+
+    private Rectangle createCard() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(60);
+        rectangle.setHeight(60);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStroke(Color.BLACK);
+
+        return rectangle;
+    }
+
+
+
+}
--- src/main/java/ui/TableController.java
+++ src/main/java/ui/TableController.java
@@ -5,7 +5,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import model.Card;
+import model.CardView;
 import model.Deck;
 import model.Hand;
 
@@ -24,6 +30,15 @@ public class TableController implements Initializable {
     @FXML
     private TextArea samHand;
 
+    @FXML
+    private HBox samCards;
+
+    @FXML
+    private HBox dealerCards;
+
+    private static final int CARD_WIDTH = 100;
+    private static final int CARD_HEIGHT = 140;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("startup");
@@ -36,6 +51,19 @@ public class TableController implements Initializable {
         dealer.addCard(newDeck.drawCard());
         samsHand.addCard(newDeck.drawCard());
         dealer.addCard(newDeck.drawCard());
+
+        CardView cardView = new CardView(dealer.getCards().get(0));
+        CardView cardView1 = new CardView(dealer.getCards().get(1));
+
+        dealerCards.getChildren().add(cardView);
+        dealerCards.getChildren().add(cardView1);
+
+        CardView cardView3 = new CardView(samsHand.getCards().get(0));
+        CardView cardView4 = new CardView(samsHand.getCards().get(1));
+
+        samCards.getChildren().add(cardView);
+        samCards.getChildren().add(cardView1);
+
         if (samsHand.isBlackjack() && dealer.isBlackjack()) {
             //Sam vinner
         }
@@ -45,6 +73,7 @@ public class TableController implements Initializable {
 
         drawCard.addEventFilter(MouseEvent.MOUSE_CLICKED, (event -> {
             samsHand.addCard(newDeck.drawCard());
+            samCards.getChildren().add( new CardView(samsHand.getCards().get(2)));
             if (samsHand.isBusted()) {
 
             }
