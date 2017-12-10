4cf4681feae8311954d50f6dd478f4d3d69f5b89
EivindS <eiv.steen@gmail.com>
2017-12-10 21:27:26 +0100

    Fjerne unødvendig spaces

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -24,9 +24,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
